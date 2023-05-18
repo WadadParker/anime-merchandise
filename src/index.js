f32460988048df6d62c8608420d53eb89014f928
Wadad Parker <wadadparker5@gmail.com>
2023-05-19 03:59:48 +0530

    refactor: Added DataBase

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter} from "react-router-dom";
 
+import { ProductContext,ProductProvider } from "./backend/context/ProductContext";
+
+export {ProductContext};
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <ProductProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </ProductProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -12,14 +12,14 @@ export const ProductPage=()=>
                 <h1>All Products</h1>
                 <ul className="products">
                     <li>
-                        <img src="" width={200} height={250}/>
+                        <img src="https://comicsense.b-cdn.net/wp-content/uploads/2023/05/tokyorevengers_blackdragon_fullsleevetee_listing3_hitanshi-800x1067.jpg" width={200} height={250}/>
                         <p>Men Premium Jacket</p>
                         <strong>Rs 200</strong>
                         <button className="go-to-cart">Go to Cart</button>
                         <button className="add-to-cart">Add t0 Cart</button>
                     </li>
                     <li>
-                    <img src="" width={200} height={250}/>
+                    <img src="https://comicsense.b-cdn.net/wp-content/uploads/2022/01/walhalla_plain_back_comicsense.jpg" width={200} height={250}/>
                         <p>Women Premium Jacket</p>
                         <strong>Rs 200</strong>
                         <button className="go-to-cart">Go to Cart</button>
