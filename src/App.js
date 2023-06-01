69f3789174f8e584a9a3792ee37c7e8baf8d21c4
Wadad Parker <wadadparker5@gmail.com>
2023-06-01 19:02:26 +0530

    refactor: project-logic

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import logo from "./logo.png";
 import {Route,Routes} from "react-router-dom";
 
-import {NavBar} from "./components/NavBar";
+import {NavBar} from "./components/navbar/NavBar";
 import Mockman from "mockman-js";
 
-import { ProductPage } from "./pages/ProductPage";
+import { ProductPage } from "./pages/productListing/ProductPage";
 import {Home} from "./pages/Home";
-import { LoginPage } from "./pages/LoginPage";
-import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/loginAndSignup/LoginPage";
+import { SignUpPage } from "./pages/loginAndSignup/SignUpPage";
 import { ProductCard } from "./pages/ProductCard";
-import { CartPage } from "./pages/CartPage";
-import { WishListPage } from "./pages/WishlistPage";
+import { CartPage } from "./pages/cartAndWishlistPage/CartPage";
+import { WishListPage } from "./pages/cartAndWishlistPage/WishlistPage";
 
 function App() {
   return (
